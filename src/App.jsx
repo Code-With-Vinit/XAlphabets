47c2b4bad93dec1083eb52176d9d531d2b263b1a
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-11-23 20:29:07 +0530

    Implemented XAlphabets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import React,{useState} from 'react';
 import './App.css'
 
 function App() {
+  const [input,setInput]=useState("");
+  const arr=('ABCDEFGHIJKLMNOPQRSTUVWXYZ').split('');
+
+  const handleInput=(letter)=>{
+    setInput(prev=>prev+letter);
+  }
+
+  const handleBackspace=()=>{
+    setInput(prev=>prev.slice(0,-1));
+  }
 
   return (
-    <>
-        <h1>XAlphabets</h1>
-    </>
+    <div className='output'>
+        <h1>Alphabet Buttons</h1>
+        <p>Click letters(or use your keyboard) to build text.</p>
+        <input  value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Your text will appear here..." type="text"/>
+        <br />
+        <button onClick={handleBackspace}>Backspace</button>
+        <br />
+        {arr.map(char=>(
+          <span key={char}>
+            <button className="key" value={char} onClick={()=>handleInput(char)}>{char}</button>
+            {char==='M'?<br />:""}
+          </span>
+        ))}
+
+    </div>
   )
 }
 
